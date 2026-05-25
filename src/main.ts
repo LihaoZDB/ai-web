113ed248478479e34a889e1ec8f0e14e99b07358
lihao <1791559969@qq.com>
2026-05-25 15:00:47 +0800

    init: 初始化项目

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import "./assets/base.css";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.use(router);
+
+app.mount("#app");
